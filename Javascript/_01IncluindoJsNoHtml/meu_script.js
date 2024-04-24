859de46f8a55be45ff996c6f4023995db6e9fa2d
Vinícius Henriques <viniciuspinholi6@gmail.com>
2024-04-23 21:22:19 -0300

    Incluindo js no html; variáveis e concatenação

---
+++ Javascript/_01IncluindoJsNoHtml/meu_script.js
@@ -0,0 +1,2 @@
+alert('Olá, seja bem vindo ao curso!')
+document.getElementById('nome').value = 'Oi' //Ex ordem de precedência, essa linha só vai funcionar se o import do script estiver depois do elemento input
